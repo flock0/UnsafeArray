5e716751529b37a48758be3e8b9cdbf25d1cdb57
Florian Chlan <florian@chlan.net>
2015-02-23 14:47:33 +0100

    Added UnsafeUtils including sizeof-Operator

---
+++ src/UnsafeUtils.java
@@ -0,0 +1,116 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+import sun.misc.Unsafe;
+
+
+public class UnsafeUtils {
+	
+	private static Unsafe unsafe;
+	
+	static {
+		try {
+			Constructor<Unsafe> unsafeConstructor = Unsafe.class.getDeclaredConstructor();
+			unsafeConstructor.setAccessible(true);
+			unsafe = unsafeConstructor.newInstance();
+		} catch (InvocationTargetException | IllegalArgumentException | IllegalAccessException | InstantiationException | NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Returns the size of the given class in bytes
+	 * 
+	 * @param cls The class whose size to get. Must not be null or an array
+	 * 
+	 * @see https://stackoverflow.com/questions/8775865/is-there-a-simple-way-to-get-the-size-of-a-java-object
+	 */
+	public static int sizeof(Class<?> cls) {
+
+        if (cls == null)
+            throw new NullPointerException();
+
+        if (cls.isArray())
+            throw new IllegalArgumentException();
+
+        if (cls.isPrimitive())
+            return primsize(cls);
+
+        int lastOffset = Integer.MIN_VALUE;
+        Class<?> lastClass = null;
+
+        for (Field f : getAllNonStaticFields(cls)) {
+            if (Modifier.isStatic(f.getModifiers()))
+                continue;
+
+            int offset = (int) unsafe.objectFieldOffset(f);
+            if (offset > lastOffset) {
+                lastOffset = offset;
+                lastClass = f.getClass();
+            }
+        }
+        if (lastOffset > 0)
+            return modulo8(lastOffset + primsize(lastClass));
+        else
+            return 16;
+    }
+	
+	/**
+	 * Returns all non-static fields of a class (including its superclasses)
+	 */
+	private static Field[] getAllNonStaticFields(Class<?> cls) {
+        if (cls == null)
+            throw new NullPointerException();
+
+        List<Field> fieldList = new ArrayList<Field>();
+        while (cls != Object.class) {
+            for (Field f : cls.getDeclaredFields()) {
+                if (!Modifier.isStatic(f.getModifiers()))
+                    fieldList.add(f);
+            }
+            cls = cls.getSuperclass();
+        }
+        Field[] fs = new Field[fieldList.size()];
+        fieldList.toArray(fs);
+        return fs;
+    }
+	
+	/**
+	 * Returns the amount of memory allocated to the class in bytes
+	 * @param cls The class whose size to get
+	 * @return The size in bytes
+	 */
+	private static int primsize(Class<?> cls) {
+        if (cls == byte.class)
+            return 1;
+        if (cls == boolean.class)
+            return 1;
+        if (cls == char.class)
+            return 2;
+        if (cls == short.class)
+            return 2;
+        if (cls == int.class)
+            return 4;
+        if (cls == float.class)
+            return 4;
+        if (cls == long.class)
+            return 8;
+        if (cls == double.class)
+            return 8;
+        else
+            return 4; // Reference pointers are assumed to be 4 byte long
+	}
+	
+	private static int modulo8(int value) {
+        return (value & 0x7) > 0 ? (value & ~0x7) + 8 : value;
+    }
+	
+	public static Unsafe getUnsafe() {
+		return unsafe;
+	}
+}
