bbf1777cf416decc82ecaf29ca07c57cfd114365
Florian Chlan <florian@chlan.net>
2015-02-25 11:59:32 +0100

    "Dirty" implementation of set-Operation

--- src/UnsafeArray.java
+++ src/UnsafeArray.java
@@ -43,62 +43,29 @@ public class UnsafeArray<E> {
 	 * @return The element at the specified index.
 	 */
 	public E get(int index) {
-		long offset = sizeOfClassInBytes * index;
-		unsafe.
-		//Object obj = unsafe.getObject(ints, offset);
-		//return type.cast(obj);
 		return null;
-		unsafe.
 	}
 
-	public void set(E obj, int index) {
+	public E set(E obj, int index) throws Exception {
 		long offset = sizeOfClassInBytes * index;
-		long offsetInObject = offset;
-		for (Field f : UnsafeUtils.getAllNonStaticFields(type)) {
-			try {
-				int fieldSize = UnsafeUtils.getFieldSize(f.getClass());
-				String datatypeName = uppercaseFirstLetter(f.getType().getName());
-				Method method = getAppropriateUnsafeMethod(datatypeName, f);
-				invokeMethod(method, datatypeName, offsetInObject, f, obj);
-				offsetInObject += fieldSize;
-				//Unsafe.class.getMethod(methodName, f.getClass()).invoke(offset, f.getClass().getField(f.getName()));
-			} catch (IllegalAccessException | IllegalArgumentException
-					| InvocationTargetException | NoSuchMethodException
-					| SecurityException | NoSuchFieldException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
+		long srcAddress = UnsafeUtils.addressOf(obj);
+		long destAddress = baseAddressInMemory + offset;
+		unsafe.copyMemory(srcAddress, destAddress, sizeOfClassInBytes);
+		System.out.println(bytesAreEqual(srcAddress, destAddress,
+				sizeOfClassInBytes));
+		Object[] helperArr = new Object[1];
+		long helperBaseOffset = unsafe.arrayBaseOffset(Object[].class);
+		long addressOfFirstHelperElement = UnsafeUtils.addressOf(helperArr) + helperBaseOffset;
+		unsafe.putLong(addressOfFirstHelperElement, destAddress);
+		return (E) helperArr[0];
+		
+	}
+
+	private boolean bytesAreEqual(long srcAddress, long destAddress, long sizeOfClassInBytes) {
+		for(int i = 0; i < sizeOfClassInBytes; i++) {
+			if(unsafe.getByte(srcAddress + i) != unsafe.getByte(destAddress + i))
+				return false;
 		}
+		return true;
 	}
-
-	private Method getAppropriateUnsafeMethod(String datatypeName, Field f) throws NoSuchMethodException, SecurityException {
-		String methodName = "put" + datatypeName;
-		if(datatypeName.equals("Boolean"))
-			return Unsafe.class.getMethod(methodName, Object.class, long.class, f.getType());
-		else
-			return Unsafe.class.getMethod(methodName, long.class, f.getType());
-	}
-	
-	private void invokeMethod(Method method, String datatypeName, long offset, Field f, E obj) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchFieldException, SecurityException, NoSuchMethodException {
-		String fieldGetMethodName = "get" + datatypeName;
-		Method fieldGetMethod = f.getClass().getMethod(fieldGetMethodName, Object.class);
-		if(datatypeName.equals("Boolean"))
-			method.invoke(unsafe, baseAddressInMemory, baseAddressInMemory + offset, fieldGetMethod.invoke(f, obj));
-		else
-			method.invoke(unsafe, baseAddressInMemory + offset, fieldGetMethod.invoke(f, obj));
-		
-		
-		
-	}
-
-	/**
-	 * Capitalizes the first letter of the input
-	 * @param input Must be at least 1 character long
-	 * @return A string with the first letter in uppercase
-	 */
-	private String uppercaseFirstLetter(String input) {
-		return input.substring(0, 1).toUpperCase() + input.substring(1);
-	}
-
 }
--- src/UnsafeUtils.java
+++ src/UnsafeUtils.java
@@ -6,97 +6,147 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+
 import sun.misc.Unsafe;
 
-
 public class UnsafeUtils {
-	
+
 	private static Unsafe unsafe;
-	
+
 	static {
 		try {
-			Constructor<Unsafe> unsafeConstructor = Unsafe.class.getDeclaredConstructor();
+			Constructor<Unsafe> unsafeConstructor = Unsafe.class
+					.getDeclaredConstructor();
 			unsafeConstructor.setAccessible(true);
 			unsafe = unsafeConstructor.newInstance();
-		} catch (InvocationTargetException | IllegalArgumentException | IllegalAccessException | InstantiationException | NoSuchMethodException | SecurityException e) {
+		} catch (InvocationTargetException | IllegalArgumentException
+				| IllegalAccessException | InstantiationException
+				| NoSuchMethodException | SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * Returns the size of the given class in bytes.
-	 * This is the sum of bytes allocated for the fields.
+	 * Returns the size of the given class in bytes
 	 * 
 	 * @param cls The class whose size to get. Must not be null or an array
-	 * @return The size of the class in bytes
+	 * 
+	 * @see https://stackoverflow.com/questions/8775865/is-there-a-simple-way-to-get-the-size-of-a-java-object
 	 */
 	public static int sizeof(Class<?> cls) {
-		int sum = 0;
-		for (Field f : getAllNonStaticFields(cls))
-			sum += getFieldSize(f.getClass());
-		return sum;
-    }
-	
-	/**
-	 * Returns all non-static fields of a class (including those from its superclasses)
-	 */
-	public static Field[] getAllNonStaticFields(Class<?> cls) {
+
         if (cls == null)
             throw new NullPointerException();
 
-        List<Field> fieldList = new ArrayList<Field>();
-        while (cls != Object.class) {
-            for (Field f : sortFieldsByName(cls.getDeclaredFields())) {
-                if (!Modifier.isStatic(f.getModifiers()))
-                    fieldList.add(f);
+        if (cls.isArray())
+            throw new IllegalArgumentException();
+
+        if (cls.isPrimitive())
+            return getFieldSize(cls);
+
+        int lastOffset = Integer.MIN_VALUE;
+        Class<?> lastClass = null;
+
+        for (Field f : getAllNonStaticFields(cls)) {
+            if (Modifier.isStatic(f.getModifiers()))
+                continue;
+
+            int offset = (int) unsafe.objectFieldOffset(f);
+            if (offset > lastOffset) {
+                lastOffset = offset;
+                lastClass = f.getClass();
             }
-            cls = cls.getSuperclass();
         }
-        Field[] fs = new Field[fieldList.size()];
-        fieldList.toArray(fs);
-        return fs;
+        if (lastOffset > 0)
+            return modulo8(lastOffset + getFieldSize(lastClass));
+        else
+            return 16;
     }
-	
+
+	/**
+	 * Returns all non-static fields of a class (including those from its
+	 * superclasses)
+	 */
+	public static Field[] getAllNonStaticFields(Class<?> cls) {
+		if (cls == null)
+			throw new NullPointerException();
+
+		List<Field> fieldList = new ArrayList<Field>();
+		while (cls != Object.class) {
+			for (Field f : sortFieldsByName(cls.getDeclaredFields())) {
+				if (!Modifier.isStatic(f.getModifiers()))
+					fieldList.add(f);
+			}
+			cls = cls.getSuperclass();
+		}
+		Field[] fs = new Field[fieldList.size()];
+		fieldList.toArray(fs);
+		return fs;
+	}
+
 	/**
 	 * Returns the amount of memory allocated to the class in bytes
-	 * @param cls The class whose size to get
+	 * 
+	 * @param cls
+	 *            The class whose size to get
 	 * @return The size in bytes
 	 */
 	public static int getFieldSize(Class<?> cls) {
-        if (cls == byte.class)
-            return 1;
-        if (cls == boolean.class)
-            return 1;
-        if (cls == char.class)
-            return 2;
-        if (cls == short.class)
-            return 2;
-        if (cls == int.class)
-            return 4;
-        if (cls == float.class)
-            return 4;
-        if (cls == long.class)
-            return 8;
-        if (cls == double.class)
-            return 8;
-        else
-            return 4; // Reference pointers are assumed to be 4 byte long
+		if (cls == byte.class)
+			return 1;
+		if (cls == boolean.class)
+			return 1;
+		if (cls == char.class)
+			return 2;
+		if (cls == short.class)
+			return 2;
+		if (cls == int.class)
+			return 4;
+		if (cls == float.class)
+			return 4;
+		if (cls == long.class)
+			return 8;
+		if (cls == double.class)
+			return 8;
+		else
+			return 4; // Reference pointers are assumed to be 4 byte long
 	}
-	
+
 	private static int modulo8(int value) {
-        return (value & 0x7) > 0 ? (value & ~0x7) + 8 : value;
-    }
+		return (value & 0x7) > 0 ? (value & ~0x7) + 8 : value;
+	}
 
 	public static Unsafe getUnsafe() {
 		return unsafe;
 	}
-	
+
+	public static long addressOf(Object o) throws Exception {
+
+		Object[] array = new Object[] { o };
+
+		long baseOffset = unsafe.arrayBaseOffset(Object[].class);
+		int addressSize = unsafe.addressSize();
+		long objectAddress;
+		switch (addressSize) {
+		case 4:
+			objectAddress = unsafe.getInt(array, baseOffset);
+			break;
+		case 8:
+			objectAddress = unsafe.getLong(array, baseOffset);
+			break;
+		default:
+			throw new Error("unsupported address size: " + addressSize);
+		}
+
+		return (objectAddress);
+	}
+
 	private static Field[] sortFieldsByName(Field[] fields) {
 		Arrays.sort(fields, new FieldNameComparator());
 		return fields;
 	}
-	
+
 	private static class FieldNameComparator implements Comparator<Field> {
 		@Override
 		public int compare(Field o1, Field o2) {
