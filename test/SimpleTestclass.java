c50a5ffec66af46f939b8b353a4b592383c34eb0
Florian Chlan <florian@chlan.net>
2015-02-25 12:43:28 +0100

    Extended SimpleTestclass to have a couple of fields

--- test/SimpleTestclass.java
+++ test/SimpleTestclass.java
@@ -1,8 +1,16 @@
 
 public class SimpleTestclass {
-	int x;
+	public int x;
+	public char b;
+	public char c;
+	public double d;
 	
-	public SimpleTestclass(int x) {
+	public SimpleTestclass(int x, char b, char c, double d) {
 		this.x = x;
+		this.b = b;
+		this.c = c;
+		this.d = d;
 	}
+	
+	
 }
